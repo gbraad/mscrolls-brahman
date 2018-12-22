07da78a6f3c2d725e26c224f39ffafe04202741f
boardedfrontdoor <marnix.home@telfort.nl>
2018-12-22 14:29:50 +0100

    cleaned up comments and added 2019 to copyright

--- xvan/dirs.c
+++ xvan/dirs.c
@@ -1,6 +1,6 @@
 
 /************************************************************************/
-/* Copyright (c) 2016, 2017, 2018 Marnix van den Bos.                   */
+/* Copyright (c) 2016, 2017, 2018, 2019 Marnix van den Bos.             */
 /*                                                                      */
 /* <marnix.home@gmail.com>                                              */
 /*                                                                      */
